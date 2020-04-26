70ddbb72927ab7fe60d062a17cdc5ede7bcac2b1
Jesús Ramos Rodríguez <jeramo02@ucm.es>
2020-04-26 22:10:21 +0200

    Minor server fix

--- ejercicio2/ejercicio2.cc
+++ ejercicio2/ejercicio2.cc
@@ -30,20 +30,20 @@ int main(int argc, char **argv)
 
     int rc = getaddrinfo(argv[1], argv[2], &hints, &res);
 
-    if ( rc != 0 )
+    if (rc != 0)
     {
-    std::cerr << "getaddrinfo error: " << gai_strerror(rc) << std::endl;
-    return -1;
+        std::cerr << "getaddrinfo error: " << gai_strerror(rc) << std::endl;
+        return -1;
     }
 
     // res contiene la representación como sockaddr de dirección + puerto
 
     int sd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
 
-    if ( bind(sd, res->ai_addr, res->ai_addrlen) != 0 )
+    if (bind(sd, res->ai_addr, res->ai_addrlen) != 0)
     {
-    std::cerr << "bind error " << std::endl;
-    return -1;
+        std::cerr << "bind error " << std::endl;
+        return -1;
     }
 
     freeaddrinfo(res);
@@ -51,75 +51,74 @@ int main(int argc, char **argv)
     // ---------------------------------------------------------------------- //
     // RECEPCIÓN MENSAJE DE CLIENTE //
     // ---------------------------------------------------------------------- //
-    char buffer [80];
+    char buffer[80];
     bool run = true;
     char host[NI_MAXHOST];
     char service[NI_MAXSERV];
     struct sockaddr client_addr;
     socklen_t client_len = sizeof(struct sockaddr);
 
-    while(run)
+    while (run)
     {
         //Evitar basura en el buffer-------------
         memset(&buffer, 0, sizeof(buffer));
         //---------------------------------------
-        ssize_t bytes = recvfrom(sd, buffer, 79 * sizeof(char) , 0, &client_addr, &client_len);
-        
+        ssize_t bytes = recvfrom(sd, buffer, 79 * sizeof(char), 0, &client_addr, &client_len);
         //Detecta si se introduce un comando válido
-        while (bytes > 2 || !isViable(buffer[0])) 
+        while (bytes > 2 || !isViable(buffer[0]) || buffer[1] != '\0')
         {
-            if ( bytes == -1)
+            if (bytes == -1)
             {
                 std::cerr << "recvfrom error " << std::endl;
                 return -1;
             }
 
             //Output de comando incorrecto/ no soportado
-            
 
-            std::cout << "Comando incorrecto..." << buffer;                   
-            std::cout <<"Los comandos aceptados son \'t\'(hora) / \'d\'(fecha) / \'q\'(terminar proceso) \n";
+
+            std::cout << "Comando incorrecto..." << buffer;
+            std::cout << "Los comandos aceptados son \'t\'(hora) / \'d\'(fecha) / \'q\'(terminar proceso) \n";
             //Evitar basura en el buffer-------------
             memset(&buffer, 0, sizeof(buffer));
             //---------------------------------------
-            bytes = recvfrom(sd, buffer, 79 * sizeof(char) , 0, &client_addr, &client_len);
+            bytes = recvfrom(sd, buffer, 79 * sizeof(char), 0, &client_addr, &client_len);
         }
 
         //Si llegó hasta aquí significa que se ha introducido un comando correcto
-        
+
         // ---------------------------------------------------------------------- //
         // PROCESAMIENTO DEL COMANDO//
         // ---------------------------------------------------------------------- //
-        
-        char respuesta [80];
-        
+
+        char respuesta[80];
+
         getnameinfo(&client_addr, client_len, host, NI_MAXHOST, service,
-        NI_MAXSERV, NI_NUMERICHOST | NI_NUMERICSERV);
+            NI_MAXSERV, NI_NUMERICHOST | NI_NUMERICSERV);
 
         std::cout << bytes << " bytes   de  " << host << ":" << service << '\n';
 
         //Procesamiento de fecha y hora y selección de respuesta
         time_t current_time;
-        struct tm * timeinfo;
+        struct tm* timeinfo;
         current_time = time(NULL);
         timeinfo = localtime(&current_time);
 
         switch (buffer[0])
         {
-            case 't':   //hora
-                strftime(respuesta,80, "%r", timeinfo);
-                break;
-            case 'd':   //fecha
-                strftime(respuesta,80, "%F", timeinfo);
-                break;
-            case 'q':   //quit
-                std::cout << "saliendo... \n";
-                run = false;
-                break;
-            default:
-                break;
+        case 't':   //hora
+            strftime(respuesta, 80, "%r", timeinfo);
+            break;
+        case 'd':   //fecha
+            strftime(respuesta, 80, "%F", timeinfo);
+            break;
+        case 'q':   //quit
+            std::cout << "saliendo... \n";
+            run = false;
+            break;
+        default:
+            break;
         }
-               
+
         // ---------------------------------------------------------------------- //
         // RESPUESTA AL CLIENTE //
         // ---------------------------------------------------------------------- //
